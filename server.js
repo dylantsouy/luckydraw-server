afb145e3ecd6e6eb8b4e5bde6805cfc483cbb4f6
DYLANTSOU\dylan <dylantsouwork@gmail.com>
2022-10-22 13:24:21 +0800

    config db

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ global.__basedir = __dirname;
 //     origin: `http://localhost:${PORT}`,
 // };
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
     next();
 });
+
+app.use(cors({ origin: 'http://localhost:3000' , credentials :  true}));
 
 app.use('/api', routes);
 
