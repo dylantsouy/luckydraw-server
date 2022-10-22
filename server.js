115c8b1bb21f3987885503def3cd587a6a143dd2
DYLANTSOU\dylan <dylantsouwork@gmail.com>
2022-10-22 14:01:37 +0800

    config db

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
-const AdminControllers = require('../controllers/admin');
 const PORT = 3000;
 const app = express();
 
@@ -10,14 +10,9 @@ global.__basedir = __dirname;
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'x-access-token, AcceptOrigin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-app.post('/signin', AdminControllers.signin);
+app.use(cors());
+
+app.use('/api', routes);
 
 app.use(logger('dev'));
 
