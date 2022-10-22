b5ca4c1607c8bf546e58c762671e89daff2ef9bf
DYLANTSOU\dylan <dylantsouwork@gmail.com>
2022-10-22 13:22:00 +0800

    config db

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ global.__basedir = __dirname;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
     next();
 });
