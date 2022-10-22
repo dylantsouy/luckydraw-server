c7bfb35634ae723cffe41a1b11083623c8872eec
DYLANTSOU\dylan <dylantsouwork@gmail.com>
2022-10-22 13:55:47 +0800

    config db

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const PORT = 3000;
 const app = express();
 
-const corsOptions = {
-    origin: `http://localhost:${PORT}`,
-};
 global.__basedir = __dirname;
 
 app.use(bodyParser.json());
 
-app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'x-access-token, AcceptOrigin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
