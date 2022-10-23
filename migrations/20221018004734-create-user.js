cd77f47fad19f6f61cdffc09ae23a255d9c0e0a3
DYLANTSOU\dylan <dylantsouwork@gmail.com>
2022-10-23 21:28:23 +0800

    fix put id

--- migrations/20221018004734-create-user.js
+++ migrations/20221018004734-create-user.js
@@ -17,6 +17,7 @@ module.exports = {
                 allowNull: false,
                 type: Sequelize.STRING(10),
                 unique: true,
+                noUpdate : false
             },
             createdAt: {
                 allowNull: false,
